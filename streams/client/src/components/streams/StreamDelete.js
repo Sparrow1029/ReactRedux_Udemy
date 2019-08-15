01fa003ee189550337f663ea7f6d63db797151ef
Sparrow1029 <sparrow@disroot.org>
2019-08-15 10:56:01 -0600

    api-server and delete functionality

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>;
-};
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamDelete;
+  renderActions() {
+    return (
+      // <>
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button
+          onClick={() => history.push('/')}
+          className="ui button"
+        >
+          Cancel
+        </button>
+      </React.Fragment>
+      // </>  ** also valid
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({ stream: state.streams[ownProps.match.params.id] });
+
+export default connect(
+  mapStateToProps,
+  { fetchStream },
+)(StreamDelete);
