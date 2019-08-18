b5132c84e6ef699909fd262bddb91dae61af4c08
Sparrow1029 <sparrow@disroot.org>
2019-08-18 13:53:23 -0600

    Added consumers and Providers to translate app.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  renderButton(color) {
+    return (
+      <button
+        className={`ui button ${color}`}
+        type="submit"
+      >
+        <LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    return <ColorContext.Consumer>{(color) => this.renderButton(color)}</ColorContext.Consumer>;
   }
 }
+
+// Alternate way to declare contextType outside class:
+// Button.contextType = LanguageContext;
 
 export default Button;
