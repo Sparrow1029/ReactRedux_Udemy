a7fb88d0334bd3caae755f82e42c7ebe76199a07
Sparrow1029 <sparrow@disroot.org>
2019-08-19 09:24:48 -0600

    Completed 'Context' language demo

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,8 +3,8 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorleggen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
   }
 
   renderButton(color) {
@@ -13,7 +13,9 @@ class Button extends React.Component {
         className={`ui button ${color}`}
         type="submit"
       >
-        <LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
       </button>
     );
   }
